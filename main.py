0464916d025c990feda7ec833a2bc5dc7024b9f2
ultroi <240160255026.akshay@gdgu.org>
2025-09-03 22:04:09 +0530

    final dealer update

--- main.py
+++ main.py
@@ -62,7 +62,6 @@ from game.pvp_system import pvp_command, pvp_callback_handler
 from game.tax_command import tax_status_command, force_tax_check_command
 from game.stats_command import stats_command, start_stats_scheduler
 from game.missions_command import missions_command, missions_callback_handler, reset_mission_command, remission_command, reset_mission_callback_handler
-from game.dealer_command import dealer_command, explore_dealer
 from game.dealer_system import handle_dealer_callback
 
 # Load environment variables
